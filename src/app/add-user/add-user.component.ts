fd71472fbf4a6d8837e48e96ed2f833a8e61c6a8
Shankar-0804 <shankarchaure2156@gmail.com>
2023-11-29 23:12:57 +0530

    created repository

---
+++ src/app/add-user/add-user.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.css']
+})
+export class AddUserComponent {
+  userData: any = {
+    id: '',
+    name: '',
+    city: '',
+    state: '',
+    country: '',
+    age: 0
+  };
+
+  constructor(private userService: UserService) { }
+
+  addUser(): void {
+    this.userService.addUser(this.userData)
+      .subscribe(
+        response => {
+          console.log('User added successfully:', response);
+          // Handle success (if needed)
+        },
+        error => {
+          console.error('Error adding user:', error);
+          // Handle error (if needed)
+        }
+      );
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
